dee8957706e136bb4689a4206d395167a7937014
tmazitov <timurmazitov000@gmail.com>
2026-06-22 19:04:13 +0400

    add predict program that using ready weights estimate the price by distance

--- cmd/predict/main.go
+++ cmd/predict/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strconv"
 
+	"github.com/tmazitov/linear_regression/internal/model"
+)
+
+func fatal(err error) {
+	fmt.Println("Error:", err)
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: predict <distance>")
+		os.Exit(1)
+	}
+
+	distance, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		fatal(fmt.Errorf("invalid distance value: %s", os.Args[1]))
+	}
+
+	m := model.NewModel()
+	if err := m.LoadWeights("./weights.json"); err != nil {
+		fatal(err)
+	}
+
+	fmt.Printf("Estimated price for %.0f km: %.2f\n", distance, m.EstimatePrice(distance))
 }
