e110b0fced44ab9f6fcc8911c318736c7a6b7f07
Timur Mazitov <timurmazitov000@gmail.com>
2026-06-23 15:39:54 +0400

    add presize programm

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 NAME_TRAIN = train
 NAME_PREDICT = predict
+NAME_PRECISION = precision
 
-all: train predict
+all: train predict precision
 
 train:
 	go build -o $(NAME_TRAIN) ./cmd/train
@@ -9,11 +10,14 @@ train:
 predict:
 	go build -o $(NAME_PREDICT) ./cmd/predict
 
+precision:
+	go build -o $(NAME_PRECISION) ./cmd/precision
+
 clean:
 	go clean
 
 fclean: clean
-	rm -f $(NAME_TRAIN) $(NAME_PREDICT)
+	rm -f $(NAME_TRAIN) $(NAME_PREDICT) $(NAME_PRECISION) weights.json
 
 re: fclean all
 
---
+++ cmd/precision/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tmazitov/linear_regression/internal/model"
+	"github.com/tmazitov/linear_regression/internal/parsing"
+)
+
+func fatal(err error) {
+	fmt.Println("Error:", err)
+	os.Exit(1)
+}
+
+func main() {
+	csvFilePath, err := parsing.GetCSVFilePath()
+	if err != nil {
+		fatal(err)
+	}
+
+	csvFile, err := parsing.NewCSVFile(csvFilePath)
+	if err != nil {
+		fatal(err)
+	}
+
+	m := model.NewModel()
+	if err := m.LoadWeights("./weights.json"); err != nil {
+		fatal(err)
+	}
+
+	prices := csvFile.Prices()
+	distances := csvFile.Distances()
+	n := len(prices)
+
+	var mape float64
+	for i := range n {
+		predicted := m.EstimatePrice(distances[i])
+		diff := prices[i] - predicted
+		if diff < 0 {
+			diff = -diff
+		}
+		mape += diff / prices[i]
+	}
+	mape = mape / float64(n) * 100
+
+	fmt.Printf("Precision: %.2f%%\n", 100-mape)
+}
